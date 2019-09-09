b0bf995385ec8d6a5cfab0d82342182464a91b67
meta-pankaj-kumar <52688948+meta-pankaj-kumar@users.noreply.github.com>
2019-09-09 19:34:35 +0530

    EADAssignment7
    09-09-2019

---
+++ Assignment7ByComponent/src/main/java/com/metacube/assignment_7/Assignment7ByComponentApplication.java
@@ -0,0 +1,22 @@
+package com.metacube.assignment_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**
+ * 
+ * @author Pankaj
+ *
+ */
+public class Assignment7ByComponentApplication {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment7ByComponentApplication.class, args);
+	}
+
+}
