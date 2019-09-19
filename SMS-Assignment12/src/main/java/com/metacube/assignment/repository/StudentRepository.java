ef59c331848056d3c04a722c62eb692c41f9f5e9
meta-pankaj-kumar <52688948+meta-pankaj-kumar@users.noreply.github.com>
2019-09-19 18:59:15 +0530

    EADAssignment12
    19-09-2019

---
+++ SMS-Assignment12/src/main/java/com/metacube/assignment/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.metacube.assignment.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.metacube.assignment.model.Student;
+
+@Repository
+public interface StudentRepository extends  JpaRepository<Student, Integer>{
+
+	List<Student> findAllByEmail(String email);
+}
